cb791499583402c769667decd276d1c3b547c23a
mahadi84 <32395595+mahadi84@users.noreply.github.com>
2025-12-17 11:16:20 +0600

    Update UploadJob.cs

--- WesterUnionPD/Models/UploadJob.cs
+++ WesterUnionPD/Models/UploadJob.cs
@@ -6,8 +6,8 @@
 public sealed class UploadJob
 {
     public Guid Id { get; set; }
-    public string Status { get; set; } = "Queued";
-    public string? Error { get; set; }   // ?=এতে কোনো ভ্যালু না-ও থাকতে পারে।
+    public string Status { get; set; } = "Queued";   //"Queued"; লেখার মানে হলো এটি একটি Default Value।
+    public string? Error { get; set; }               // ?=এতে কোনো ভ্যালু না-ও থাকতে পারে।
 
     public long ProcessedRows { get; set; }
 
